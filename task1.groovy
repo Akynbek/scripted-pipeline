ef7c9c04dc5a4aa0a0ee576c686b3d61d3b702a6
Akynbek <akynbek@gmail.com>
2020-08-28 04:42:06 +0000

    final update added

--- task1.groovy
+++ task1.groovy
@@ -7,7 +7,7 @@ if (nodeIP?.trim()) {
     node {
         withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master-ssh-key', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
             stage('Git Pull') {
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} git clone https://github.com/ikambarov/melodi.git'
+                git changelog: false, poll: false, url: 'https://github.com/ikambarov/melodi.git'
             }
             stage('Install Apache') {
                 sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} yum install httpd -y'
@@ -19,7 +19,10 @@ if (nodeIP?.trim()) {
                 sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} systemctl enable httpd'
             }
             stage("Copy Files") {
-                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} mv /root/melodi/* /var/www/html/'
+                sh 'scp -r -o StrictHostKeyChecking=no -i $SSHKEY *  $SSHUSERNAME@${nodeIP}:/var/www/html/'
+            }
+            stage("Change ownership") {
+                sh 'ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${nodeIP} chown -R apache:apache /var/www/html'
             }
             stage("Clean Workspace"){
                 'cleanWs()'
