d2dc2140a0311207070a9dca2c497b9d45269629
Akynbek <akynbek@gmail.com>
2020-09-02 00:21:53 +0000

    updated file

--- terraform-vpc-with-variable-region.groovy
+++ terraform-vpc-with-variable-region.groovy
@@ -21,37 +21,34 @@ node{
         git url: 'https://github.com/Akynbek/terraform-vpc.git'
     }
     withCredentials([usernamePassword(credentialsId: 'jenkins-aws-access-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
-        stage("Terrraform Init"){
-            sh """
-                bash setenv.sh ${environment}.tfvars
-                export AWS_REGION=${aws_region_var}
-                echo \$AWS_REGION
-                terraform-0.13 init
-                terraform-0.13 plan -var-file ${environment}.tfvars
-            """
+        withEnv(["AWS_REGION=${aws_region_var}"]) {
+            stage("Terrraform Init"){
+                sh """
+                    bash setenv.sh ${environment}.tfvars
+                    terraform-0.13 init
+                """
+            }        
+            if (terraform_apply.toBoolean()) {
+                stage("Terraform Apply"){
+                    sh """
+                        terraform-0.13 apply -var-file ${environment}.tfvars -auto-approve
+                    """
+                }
+            }
+            else if (terraform_destroy.toBoolean()) {
+                stage("Terraform Destroy"){
+                    sh """
+                        terraform-0.13 destroy -var-file ${environment}.tfvars -auto-approve
+                    """
+                }
+            }
+            else {
+                stage("Terraform Plan"){
+                    sh """
+                        terraform-0.13 plan -var-file ${environment}.tfvars
+                    """
+                }
+            }
         }        
-        if (terraform_apply.toBoolean()) {
-            stage("Terraform Apply"){
-                sh """
-                    export AWS_REGION=${aws_region_var}
-                    terraform-0.13 apply -var-file ${environment}.tfvars -auto-approve
-                """
-            }
-        }
-        else if (terraform_destroy.toBoolean()) {
-            stage("Terraform Destroy"){
-                sh """
-                    export AWS_REGION=${aws_region_var}
-                    terraform-0.13 destroy -var-file ${environment}.tfvars -auto-approve
-                """
-            }
-        }
-        else {
-            stage("Uknown"){
-                sh """
-                    echo "Choose either apply or destroy"
-                """
-            }
-        }
     }    
 }
