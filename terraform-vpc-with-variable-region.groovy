0ec3cd9b5e6793a72e9302ad09b470e09ed72d51
Akynbek <akynbek@gmail.com>
2020-09-02 00:05:50 +0000

    file updated

--- terraform-vpc-with-variable-region.groovy
+++ terraform-vpc-with-variable-region.groovy
@@ -17,6 +17,7 @@ else if(params.environment == "prod"){
 }
 node{
     stage("Pull Repo"){
+        cleanWs()
         git url: 'https://github.com/Akynbek/terraform-vpc.git'
     }
     withCredentials([usernamePassword(credentialsId: 'jenkins-aws-access-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
